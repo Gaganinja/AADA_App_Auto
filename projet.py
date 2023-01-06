c08df182ff3298da6f9d85131db581fa96277d63
Manoj Alagesan <alagesan.manoj@gmail.com>
2023-01-06 09:03:11 +0100

    imports

--- projet.py
+++ projet.py
@@ -1,3 +1,11 @@
-#Bonjour hein
-#test modification 
-#Test modif
+### import 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error,r2_score
